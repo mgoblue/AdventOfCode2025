acd089ded35be7e57a5e279bed7de473559498fb
Eugin Kim <e.kim-leighton@techsmith.com>
2025-12-06 11:41:49 -0500

    part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,41 +1,52 @@
+#![allow(unused_variables)]
 use std::fs;
 
 // part 1 - 17359
+// part 2 - 172787336861064
 fn main() {
     let contents = fs::read_to_string("input.txt")
         .expect("Failed to read input.txt");
 
-    let mut sum_jolatage: i128 = 0;
-    for line in contents.lines() {
-        //println!("Line: {}", line);
+    let mut sum_jolatage: i64 = 0;
 
-        let mut joltage = 0;
-        let mut largest = 0;
-        let mut index = 0 as usize;
-
-        for i in 0..line.len()-1 {
-            //print!("{} ", line.chars().nth(i).unwrap());
-
-            let num = line.chars().nth(i).unwrap().to_digit(10).unwrap();
-            if num > largest {
-                largest = num;
-                index = i;
-            }
-        }
-        joltage = largest * 10;
-        
-        largest = 0;
-        for i in index + 1 ..line.len() {
-            let num = line.chars().nth(i).unwrap().to_digit(10).unwrap();
-            if num > largest {
-                largest = num;
-            }
-        }
-        joltage += largest;
-        
-        //println!("=> JOLTAGE: {}", joltage);
-        //println!();
-        sum_jolatage += joltage as i128;
+    for number_str in contents.lines() {
+        sum_jolatage += find_joltage( number_str );
     }
+
     println!("Sum of joltages: {}", sum_jolatage);
+}
+
+fn find_joltage( number_str : &str ) -> i64{
+    //println!("Line: {}", number_str);
+
+    let mut joltage: i64 = 0;
+    let number_of_digits = 12;
+
+    let str_len = number_str.len() as i32;
+    let mut start_index = 0;
+    //println!("String length: {}", str_len);
+    for i in (0..number_of_digits).rev() {
+        let up_to_index = str_len - i;
+        //println!("Iteration: {}, up_to_len: {}", i, up_to_len);
+        let largest_digit = find_largest_digit( number_str, &mut start_index, up_to_index );
+        //println!("Largest digit in iteration {}: {}", i, largest_digit);
+
+        joltage = joltage * 10 + largest_digit as i64;
+    }
+    //println!("=> Current joltage: {}", joltage);
+    return joltage;
+}
+
+fn find_largest_digit( number_str: &str, start_index: &mut i32, up_to_index: i32 ) -> i32 {
+    //println!("Finding largest digit from index {} to {}", *start_index, up_to_index);
+    let mut largest: i32 = 0;
+    for i in *start_index..up_to_index {
+        let num: i32 = number_str.chars().nth(i as usize).unwrap().to_digit(10).unwrap() as i32;
+        if num > largest {
+             largest = num;
+             *start_index = i + 1;
+        }
+    }
+    //println!("=> Largest digit found: {} at index {}", largest, start_index);
+    return largest;
 }
