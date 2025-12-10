00bb6eb7050e84a8bc0ab29ddc7aaedf97ba5c23
Eugin Kim <e.kim-leighton@techsmith.com>
2025-12-10 07:20:42 -0500

    part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,45 +1,87 @@
 #![allow(non_snake_case)]
 #![allow(unused_mut)]
 #![allow(unused_variables)]
-
 use std::fs;
 
 // part 1 - 5524274308182
+// part 2 - 8843673199391
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
 
-    let mut numbers = Vec::<Vec::<i64>>::new();
+    let mut worksheet = Vec::<Vec::<char>>::new();
 
-    for i in 0..contents.lines().count()-1 {
-        numbers.push(Vec::<i64>::new());
-        let line = contents.lines().nth(i).unwrap();
-        let mut numberStrings : Vec::<&str> = line.split_whitespace().collect();
-        for numberString  in &numberStrings {
-            let num = numberString.parse::<i64>().unwrap();
-            numbers[i].push(num);
-        }
+    for line in contents.lines() {
+        let chars: Vec<char> = line.chars().collect();
+        worksheet.push(chars);
     }
 
-    let operators: Vec<char> = contents.lines().last().unwrap()
-    .chars()
-    .filter(|c| !c.is_whitespace())
-    .collect();
+    let mut operator = worksheet.last().unwrap()[0];
+     for i in 0..worksheet[0].len(){
+        if worksheet.last().unwrap()[i] != ' ' {
+            operator = worksheet.last().unwrap()[i];
+        } else {
+           worksheet.last_mut().unwrap()[i] = operator;
+        }
+    }
+    //println!( "operators {:?}", worksheet.last().unwrap() );
+    //println!( "worksheet {:?}", worksheet);
 
     let mut sum = 0;
-    for i in 0..operators.len() {
-        let mut result = numbers[0][i];
-        for j in 1..numbers.len() {
-            let operator = operators[i];
-            let nextNumber = numbers[j][i];
-            match operator {
-                '+' => result += nextNumber,
-                '-' => result -= nextNumber,
-                '*' => result *= nextNumber,
-                '/' => result /= nextNumber,
-                _ => (),
-            }
-        }
-        sum += result;
+    let mut operator = worksheet.last().unwrap()[0];
+    let mut result: i128 = 0;
+    if operator == '*'{
+       result = 1;
     }
-    println!("sum {}", sum);
+    for i in 0..worksheet[0].len() {
+        let col = worksheet[..worksheet.len()-1].iter().map(|row| row[i]).collect::<Vec<char>>();
+        if !hasNumber(&col) {
+            println!("result {}", result);
+            sum += result;
+            operator = worksheet.last().unwrap()[i+1];
+            result = 0;
+            if operator == '*'{
+                result = 1;
+            }
+
+            continue;
+        }
+
+        let number = findNumber(&col);
+        //println!("Column {:?} operator {}", col, operator);
+
+        match operator {
+            '+' => result += number,
+            '*' => result *= number,
+            _ => (),
+        }
+        //println!("number {} result {}", number, result);
+    }
+    sum += result;
+    println!("final result {}", sum);
+    // let mut sum = 0;
+
+    //     sum += result;
+    // }
+    // println!("sum {}", sum);
+}
+
+fn findNumber(col: &Vec<char>) -> i128 {
+    let mut number_str = String::new();
+    for &c in col {
+        if c.is_digit(10) {
+            number_str.push(c);
+        }
+    }
+    let number: i128 = number_str.parse().unwrap();
+    println!("Found number {}", number);
+    return number;
+}
+
+fn hasNumber(col: &Vec<char>) -> bool {
+    for &c in col {
+        if c.is_digit(10) {
+            return true;
+        }
+    }
+    return false;
 }
